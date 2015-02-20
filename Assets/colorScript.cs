011d8c94da674274e10a6f02beab8942db4d33c2
kerama19 <kenneth.ramirez@proximitycr.com>
2015-02-20 12:14:26 -0600

    Full Functional Painting Cube

---
+++ Assets/colorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorScript : MonoBehaviour {
+
+	public void paint()
+	{
+		Color randomColor = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), 1.0f);
+		renderer.material.color = randomColor;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
